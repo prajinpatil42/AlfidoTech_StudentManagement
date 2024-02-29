e071537c39e5e32656bd9b2c41495f1263b89900
prajin <prajinpatil42@gmail.com>
2024-02-29 18:24:26 +0530

    initial commit

---
+++ src/main/java/com/Student/StudentManagementApplication.java
@@ -0,0 +1,15 @@
+package com.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+	}
+}
+
+
+
